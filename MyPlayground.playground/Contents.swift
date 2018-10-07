3d8511292c2ce9309f0f8ccfffccb22239bcd078
Patrick Temple <ptemple@butterflynetwork.com>
2018-10-07 17:52:11 -0400

    Add a playground to try out Swift statements

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "A new value"
+
+let constStr = "Pi is 3.14"
+
+var temperature: Float
+var isMonday: Bool
+
+var arrayOfInts: [Int]
+var dictionaryOfCapitalsByCountry: [String:String]
+
+var winningLotteryNumbers: Set<Int>
+
+let number = 42
+let fmStation = 91.1
+
+let countingUp = ["one", "two"]
+let nameByParkingSpace = [13: "Alice", 27: "Bob"]
+
+let secondElement = countingUp[1]
+
+let emptyString = String()
+let emptyArrayOfInts = [Int]()
+let emptySetOfFloats = Set<Float>()
+
+let defaultNumber = Int()
+let defaultBool = Bool()
+let meaningOfLife = String(number)
+
+let availableRooms = Set([205, 411, 412])
+
+let defaultFloat = Float()
+let floatFromLiteral = Float(3.14)
+let easyPi = 3.14
+let floatFromDouble = Float(easyPi)
+
+let floatingPi: Float = 3.14
+
+countingUp.count
+emptyString.isEmpty
+
+countingUp.append("three")
